b9ecf5c38166666a57cb489172f462bb7ba560af
Franz Deschler <webmaster@codestore.cloud>
2026-04-29 21:56:16 +0200

    Add database migration for ArtistArtwork

---
+++ src/app/data/migrations/migration10.ts
@@ -0,0 +1,18 @@
+import { Migration } from '../migration';
+
+export class Migration10 extends Migration {
+    public id: number = 10;
+    public name: string = 'Migration10';
+
+    public up(): void {
+        this.sql(`CREATE TABLE ArtistArtwork (
+                        ArtistArtworkID	    INTEGER,
+                        ArtistKey	        TEXT,
+                        ArtworkID	        TEXT,
+                        PRIMARY KEY(ArtistArtworkID));`);
+
+        this.sql('ALTER TABLE Track ADD ArtistKey TEXT;');
+        this.sql('ALTER TABLE Track ADD NeedsArtistArtworkIndexing INTEGER;');
+        this.sql('UPDATE Track SET NeedsArtistArtworkIndexing=1;');
+    }
+}
